6f80696d1fbbc5035c47420ecc0cffdb7f76959d
Inacio Silva <joseinaciolds@gmail.com>
2024-04-09 19:09:44 -0300

    listagem de transação

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,24 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return transactions
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transactions = await knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactioBodySchema = z.object({
       title: z.string(),
