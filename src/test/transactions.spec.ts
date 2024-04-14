7fcd16101122e3040d71cc0b7d9aef34c7d82ac6
Inacio Silva <joseinaciolds@gmail.com>
2024-04-14 18:00:49 -0300

    categorizando os testes

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,26 @@
+import { afterAll, beforeAll, it, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    // fazer uma chamada HTTP para criar uma nova transação
+
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
